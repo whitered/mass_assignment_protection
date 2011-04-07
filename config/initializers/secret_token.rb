71b34164cf1288ade3b70aceb4649fb36bb52c25
whitered <dmitry.zhelnin@gmail.com>
2011-04-07 17:18:38 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MassAssignment::Application.config.secret_token = 'a706e167f0f376314ea73a350a95e7d05b9ffcd07785464cafb4bc244733cb0d63b3c2fc6a53bed040e8eabe1e45d68d8c77c7262e850c3da0fb9b73a74a69a0'
